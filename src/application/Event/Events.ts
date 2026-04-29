9dcf8f123ef6259387d2a2074c9e0bfc82dc3e82
Diego Sousa <diegointeriores11@gmail.com>
2026-04-29 10:09:15 -0300

    refactor: events

--- src/application/Event/Events.ts
+++ src/application/Event/Events.ts
@@ -2,13 +2,21 @@ import type { OrderDTO } from "../Order/dtos/OrderDto";
 
 export namespace Events {
     export enum Source {
-        Orders = "ecommerce.orders"
+        Orders = "cede.orders"
     }
+
     export enum Names {
-        OrderCreated = "order.created",
-        OrderCancelled = "order.cancelled",
-        OrderPaid = "order.paid"
+        OrderCreated = "OrderCreated.v1",
+        OrderCancelled = "OrderCancelled.v1",
+        OrderPaid = "OrderPaid.v1"
     }
+
+    export enum Queues {
+        NotifyUser = "notification-user-order",
+        NotifyWarehouse = "warehouse-order-processing",
+        ReserveInventory = "inventory-reservation"
+    }
+
     export interface Mappings {
         [Events.Names.OrderCreated]: OrderDTO;
         [Events.Names.OrderCancelled]: OrderDTO;
