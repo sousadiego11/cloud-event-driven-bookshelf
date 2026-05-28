5dd9e1724b334b07ee853c82bca922e3206aebbe
Diego Sousa <diegointeriores11@gmail.com>
2026-05-28 13:35:14 -0300

    feat: add archive_file data source for Lambda functions; update IAM role policy and remove obsolete S3 bucket and dummy lambda zip

--- terraform/aws_lambdas.tf
+++ terraform/aws_lambdas.tf
@@ -1,3 +1,9 @@
+data "archive_file" "app" {
+  type        = "zip"
+  source_dir  = "${path.module}/../dist"
+  output_path = "${path.module}/../dist/app.zip"
+}
+
 data "aws_iam_policy_document" "lambda_policy_doc" {
 
   # Logs (CloudWatch)
@@ -53,13 +59,10 @@ data "aws_iam_policy_document" "lambda_policy_doc" {
       "sqs:SendMessage"
     ]
 
-    resources = [aws_sqs_queue.notify_library_book_registered.arn]
+    resources = [
+      aws_sqs_queue.notify_library_book_registered.arn
+    ]
   }
-}
-
-resource "aws_iam_role_policy" "lambda_policy" {
-  role   = aws_iam_role.lambda_role.id
-  policy = data.aws_iam_policy_document.lambda_policy_doc.json
 }
 
 resource "aws_iam_role" "lambda_role" {
@@ -67,52 +70,84 @@ resource "aws_iam_role" "lambda_role" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
+
     Statement = [
       {
         Effect = "Allow"
+
         Principal = {
           Service = "lambda.amazonaws.com"
         }
+
         Action = "sts:AssumeRole"
       }
     ]
   })
 }
 
+resource "aws_iam_role_policy" "lambda_policy" {
+  role   = aws_iam_role.lambda_role.id
+  policy = data.aws_iam_policy_document.lambda_policy_doc.json
+}
+
 resource "aws_lambda_function" "register_book" {
   function_name = "bookshelf-register-book"
 
-  filename         = "${path.module}/dummy-lambda.zip"
-  source_code_hash = filebase64sha256("${path.module}/dummy-lambda.zip")
+  filename         = data.archive_file.app.output_path
+  source_code_hash = data.archive_file.app.output_base64sha256
 
   handler = "aws-apigateway/register_book.handler"
   runtime = "nodejs20.x"
 
   role = aws_iam_role.lambda_role.arn
+
+  depends_on = [
+    aws_iam_role_policy.lambda_policy
+  ]
+
+  tags = {
+    Name = "RegisterBookFunction"
+  }
 }
 
 resource "aws_lambda_function" "register_loan" {
   function_name = "bookshelf-register-loan"
 
-  filename         = "${path.module}/dummy-lambda.zip"
-  source_code_hash = filebase64sha256("${path.module}/dummy-lambda.zip")
+  filename         = data.archive_file.app.output_path
+  source_code_hash = data.archive_file.app.output_base64sha256
 
   handler = "aws-apigateway/register_loan.handler"
   runtime = "nodejs20.x"
 
   role = aws_iam_role.lambda_role.arn
+
+  depends_on = [
+    aws_iam_role_policy.lambda_policy
+  ]
+
+  tags = {
+    Name = "RegisterLoanFunction"
+  }
 }
 
 resource "aws_lambda_function" "notify_library_book_registered" {
   function_name = "bookshelf-notify-library-book-registered"
 
-  filename         = "${path.module}/dummy-lambda.zip"
-  source_code_hash = filebase64sha256("${path.module}/dummy-lambda.zip")
+  filename         = data.archive_file.app.output_path
+  source_code_hash = data.archive_file.app.output_base64sha256
 
   handler = "aws-sqs/notify_library_book_registered.handler"
   runtime = "nodejs20.x"
 
   role = aws_iam_role.lambda_role.arn
+
+  depends_on = [
+    aws_iam_role_policy.lambda_policy
+  ]
+
+  tags = {
+    Name = "NotifyLibraryBookRegisteredFunction"
+  }
 }
 
 resource "aws_lambda_event_source_mapping" "notify_library_book_registered_mapping" {
--- terraform/aws_s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "lambdas" {
-  bucket = "bookshelf-lambdas-bucket"
-
-  tags = {
-    Name        = "Bookshelf Lambdas Bucket"
-    Environment = "Production"
-  }
-}
